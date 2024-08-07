0f0d74492df6ba462f278f582e04eb8154c0ca1a
Sipky <sipky.jaya@bluebirdgroup.com>
2024-08-08 00:48:52 +0700

    fix update

--- models/commercial_invoice.go
+++ models/commercial_invoice.go
@@ -1,12 +1,29 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type CommercialInvoice struct {
 	gorm.Model
 	InvoiceNo       string    `json:"invoice_no"`
+	IssuerName      string    `json:"issuer_name"`
+	IssuerAddress   string    `json:"issuer_address"`
+	IssuerEmail     string    `json:"issuer_email"`
+	IssuerPhone     string    `json:"issuer_phone"`
+	ReceiverName    string    `json:"receiver_name"`
+	ReceiverAddress string    `json:"receiver_address"`
+	ReceiverEmail   string    `json:"receiver_email"`
+	ReceiverPhone   string    `json:"receiver_phone"`
+	NotifyParty     bool      `json:"notify_party"`
+	PartyName       string    `json:"party_name"`
+	PartyAddress    string    `json:"party_address"`
+	PartyEmail      string    `json:"party_email"`
+	PartyPhone      string    `json:"party_phone"`
+	NoSC            *string   `json:"no_sc"`
+	Date            time.Time `json:"date"`
 	TransportDetail string    `json:"transport_detail"`
 	PaymentTermID   uint      `json:"payment_term_id"`
 	Products        []Product `json:"products"`
--- models/packing_list.go
+++ models/packing_list.go
@@ -1,27 +1,13 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type PackingList struct {
 	gorm.Model
-	PackingListNo       *string   `json:"packing_list_no"`
-	IssuerName          string    `json:"issuer_name"`
-	IssuerAddress       string    `json:"issuer_address"`
-	IssuerEmail         string    `json:"issuer_email"`
-	IssuerPhone         string    `json:"issuer_phone"`
-	ReceiverName        string    `json:"receiver_name"`
-	ReceiverAddress     string    `json:"receiver_address"`
-	ReceiverEmail       string    `json:"receiver_email"`
-	ReceiverPhone       string    `json:"receiver_phone"`
-	NoSC                string    `json:"no_sc"`
-	Date                time.Time `json:"date"`
-	TotalPackages       int       `json:"total_packages"`
-	TotalPackagesName   string    `json:"total_packages_name"`
-	TotalGrossWeight    float64   `json:"total_gross_weight"`
-	TotalNettWeight     float64   `json:"total_nett_weight"`
-	CommercialInvoiceID uint      `json:"commercial_invoice_id"`
+	PackingListNo       *string `json:"packing_list_no"`
+	TotalPackages       int     `json:"total_packages"`
+	TotalPackagesName   string  `json:"total_packages_name"`
+	CommercialInvoiceID uint    `json:"commercial_invoice_id"`
 }
